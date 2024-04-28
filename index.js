51550b174d3cc35e478ee0812203f554307e50d5
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-04-28 23:58:52 +0600

    API created for get single data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -9,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.SPOT_USER}:${process.env.SPOT_PASS}@cluster0.fxbdhbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +34,14 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
-        
+
+        // get single spot 
+        app.get('/homeSpot/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await spotCollection.findOne(query);
+            res.send(result)
+        })
 
         // Post user
         app.post('/userAddedSpot', async(req, res)=>{
